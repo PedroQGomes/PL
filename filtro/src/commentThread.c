738f5c7e162f27d2d30420022ba04cad167dfcc8
PedroQGomes <pedromqueiros1999@gmail.com>
2020-04-04 21:35:38 +0100

    merges

--- filtro/src/commentThread.c
+++ filtro/src/commentThread.c
@@ -13,7 +13,7 @@ typedef struct commentThread {
     GString * date;
     GString * timestamp; 
     GString * comentTxt; 
-    int likes; 
+    GString * likes; 
     int hasReplies;
     int numberOfReplies;
     CommentThread replies[]; 
@@ -29,7 +29,7 @@ CommentThread newCommentThread(){
     ct->date = g_string_new("");
     ct->timestamp = g_string_new(""); 
     ct->comentTxt = g_string_new("");
-    ct->likes = 0;
+    ct->likes = g_string_new("");
     ct->hasReplies = FALSE;
     ct->numberOfReplies = 0; 
     //ct->replies
@@ -44,11 +44,10 @@ void freeCommentThread(CommentThread c){
     g_string_free(c->date,TRUE);
     g_string_free(c->timestamp,TRUE);
     g_string_free(c->comentTxt,TRUE);
-    c->likes = 0;
+    
     c->hasReplies = FALSE;
     c->numberOfReplies = 0;
     //g_list_free(c->replies);
-    
 
 
 }
@@ -81,8 +80,8 @@ void addCommentTxt(CommentThread c,char* s){
 }
 
 
-void addLikes(CommentThread c,int l){
-    c->likes += l;
+void addLikes(CommentThread c,char* s){
+    //g_string_append(c->likes,s);
 }
 
 void setHasReplaiesTRUE(CommentThread c){
